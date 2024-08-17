79623d3158eb2468c33d6f93314e43c4ac49fdf3
Nenad <nbursa@gmail.com>
2024-08-17 21:13:50 +0200

    Document some code

--- src/store/search.ts
+++ src/store/search.ts
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia';
 import axios from 'axios';
 import { Criteria, Result } from '../types';
 
+// Environment variables are loaded at build time and prepared for easier maintanance
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const MARKETPLACE_ID = import.meta.env.VITE_MARKETPLACE_ID;
 const LOCALE = import.meta.env.VITE_LOCALE;
@@ -24,6 +25,7 @@ export const useSearchStore = defineStore('search', {
         this.loading = true;
         this.error = '';
 
+        // Perform an anonymous login to get a JWT token
         const response = await axios.post(`${API_BASE_URL}/loginAnonymously`);
         this.jwtToken = response.data.jwt_token;
       } catch (error) {
@@ -35,12 +37,14 @@ export const useSearchStore = defineStore('search', {
     },
 
     async searchRestaurants(criteria: Criteria) {
+      // Ensure a valid JWT token is available
       if (!this.jwtToken) {
         await this.loginAnonymously();
       }
       this.loading = true;
       this.error = '';
 
+      // Convert date and time to API-required format
       const formattedCriteria = {
         date: criteria.date.replace(/-/g, ''),
         time: criteria.time.replace(/:/g, ''),
@@ -48,6 +52,7 @@ export const useSearchStore = defineStore('search', {
       };
 
       try {
+        // Request a search token with formatted criteria
         const response = await axios.post(
           `${API_BASE_URL}/search_token`,
           {
@@ -62,8 +67,8 @@ export const useSearchStore = defineStore('search', {
             },
           },
         );
-        this.searchId = response.data.search_id;
-        await this.fetchResults();
+        this.searchId = response.data.search_id; // Save the search ID for subsequent requests
+        await this.fetchResults(); // Fetch initial search results
       } catch (error) {
         console.error('Error:', error);
         this.error = 'Failed to search restaurants.';
@@ -77,6 +82,7 @@ export const useSearchStore = defineStore('search', {
         this.loading = true;
         this.error = '';
 
+        // Request search results using the search ID
         const response = await axios.post(
           `${API_BASE_URL}/search_request`,
           { search_id: this.searchId },
@@ -88,6 +94,7 @@ export const useSearchStore = defineStore('search', {
         );
         this.results = response.data.posts;
         this.totalResults = response.data.total;
+        // Determine if more results can be fetched
         this.canLoadMore = this.results.length < this.totalResults;
       } catch (error) {
         console.error('Error:', error);
@@ -102,6 +109,7 @@ export const useSearchStore = defineStore('search', {
       this.error = '';
 
       try {
+        // Request additional search results
         const response = await axios.post(
           `${API_BASE_URL}/search_request`,
           { search_id: this.searchId },
@@ -112,10 +120,10 @@ export const useSearchStore = defineStore('search', {
           },
         );
         if (response.data.posts.length < 1) {
-          this.canLoadMore = false;
+          this.canLoadMore = false; // No more results available
           return;
         }
-        this.results = [...this.results, ...response.data.posts];
+        this.results = [...this.results, ...response.data.posts]; // Append new results to existing ones
         this.canLoadMore = this.results.length < this.totalResults;
       } catch (error) {
         console.error('Error:', error);
